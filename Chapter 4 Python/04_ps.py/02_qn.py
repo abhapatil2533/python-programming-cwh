ba40ef11625e13f270d2c94028ac403a668341c3
abhapatil2533 <abha.a.patil@gmail.com>
2024-08-28 21:55:58 +0530

    Add files via upload

---
+++ Chapter 4 Python/04_ps.py/02_qn.py
@@ -0,0 +1,9 @@
+m1 = input("Enter marks of student 1:")
+m2 = input("Enter marks of student 2:")
+m3 = input("Enter marks of student 3:")
+m4 = input("Enter marks of student 4:")
+m5 = input("Enter marks of student 5:")
+m6 = input("Enter marks of student 6:")
+marks = [m1, m2, m3, m4, m5, m6]
+marks.sort()
+print(marks)
---
+++ Chapter 4 Python/04_ps.py/03_qn.py
@@ -0,0 +1,4 @@
+a = [5, 10, 15, 20]
+add = (a[0]+ a[1] +a[2] +a[3])
+print(add)
+print(sum(a))
