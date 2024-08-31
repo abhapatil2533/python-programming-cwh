4af2e2155f621611ec35069223dd2352e22dd8c6
abhapatil2533 <abha.a.patil@gmail.com>
2024-08-31 19:41:07 +0530

    Add files via upload

---
+++ Chapter 5 Python/05_ps.py/01_qn.py
@@ -0,0 +1,8 @@
+dict = {
+    "aam": "mango",
+    "kela": "banana",
+    "santra": "orange"
+}
+
+word = input("Enter your hindi word:")
+print(dict[word])
---
+++ Chapter 5 Python/05_ps.py/05_qn.py
@@ -0,0 +1,17 @@
+favlang = {}
+a = input("Enter your fav language Bhavna:")
+b = input("Enter your fav language Archana:")
+c = input("Enter your fav language Sangita:")
+d = input("Enter your fav language Shaila:")
+
+favlang["Bhavna"] = a
+favlang["Archana"] = b
+favlang["Sangita"] = c   
+
+
+#if sangita becomes bhavna, then c will be the latest value corresponding to bhavna
+
+
+favlang["Shaila"] = d
+
+print(favlang)
