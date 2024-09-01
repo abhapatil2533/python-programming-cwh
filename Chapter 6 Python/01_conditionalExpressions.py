92c84d776b72c3010de5c2cf509dad66c375d4a3
abhapatil2533 <abha.a.patil@gmail.com>
2024-09-01 13:15:35 +0530

    Add files via upload

---
+++ Chapter 6 Python/01_conditionalExpressions.py
@@ -0,0 +1,9 @@
+a = 45
+
+if(a>10):
+    print("The value of a is greater than 10")
+if(a>30):
+    print("The value of a is greater than 30")
+else:
+    print("the value is not greater than a")
+print("done")
---
+++ Chapter 6 Python/03_logical and relational operations.py
@@ -0,0 +1,9 @@
+"""a = None
+if(a is None):
+    print("yes")
+else:
+    print("No")
+"""
+a = [3,4,5]
+print(4 in a)
+ 
