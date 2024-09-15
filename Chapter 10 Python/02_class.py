01fe313618f5747cf331c7589367879cd3c62490
abhapatil2533 <abha.a.patil@gmail.com>
2024-09-15 20:39:32 +0530

    Add files via upload

---
+++ Chapter 10 Python/02_class.py
@@ -0,0 +1,14 @@
+class Employee:
+    company = "Google"
+    salary=100
+ram= Employee()
+sham=Employee()
+ram.company="yahoo"
+ram.salary=300
+sham.company="hcl"
+sham.salary=500
+print(ram.company)
+print(ram.salary)
+print(sham.company)
+print(sham.salary)
+
---
+++ Chapter 10 Python/04_class.py
@@ -0,0 +1,12 @@
+class Employee:
+    company="Google"
+    def getsalary(self):
+        print(f"salary is {self.salary}")
+    
+    def greet():
+        print("hello sir")
+    
+obj=Employee()
+obj.salary=500
+obj.getsalary()
+obj.greet()    #Employee.greet
